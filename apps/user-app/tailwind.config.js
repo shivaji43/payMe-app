6e1c3e536faea1f4e0323a5fae268acf4adf0254
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-10-16 23:04:09 +0530

    prettier run

--- apps/user-app/tailwind.config.js
+++ apps/user-app/tailwind.config.js
@@ -5,11 +5,10 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
